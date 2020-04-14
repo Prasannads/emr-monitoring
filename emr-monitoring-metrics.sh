66b251b3867a2c1406f0d187e116f759e7341f10
Prasanna Dhongudi Sudhindrakumar <p.sudhindrakumar@free-now.com>
2020-04-14 19:34:34 +0200

    fix

--- emr-monitoring-metrics.sh
+++ emr-monitoring-metrics.sh
@@ -13,19 +13,19 @@ fi
 
 pushgateway=https://localhost:9091/metrics/job/"\$1"/instance/
 mem=($(free -m | awk -v RS="" '{print $8} {print $9} {print $10}'))
-cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${namespace_prefix}
+cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${instance_type_prefix}
     memory_total ${mem[0]}
     memory_free ${mem[1]}
     memory_used ${mem[2]}
 EOF
 cpu=($(top -b -n1 | grep "Cpu(s)" | awk '{print $2} {print $3} {print $5}' | sed 's/[^0-9.]*//g'))
-cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${namespace_prefix}
+cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${instance_type_prefix}
     cpu_user_load ${cpu[0]}
     cpu_sys_load ${cpu[1]}
     cpu_idle_load ${cpu[2]}
 EOF
 disk=($(df -hP /mnt | awk '{print $2} {print $3} {print $4} {print $5}' | tail -4 | sed 's/[^0-9.]*//g'))
-cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${namespace_prefix}
+cat << EOF | curl --data-binary @- ${pushgateway}${instance_ip}/type/${instance_type_prefix}
     disk_size ${disk[0]}
     disk_used_size ${disk[1]}
     disk_available_size ${disk[2]}
